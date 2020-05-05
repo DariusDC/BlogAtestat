61f6425b19713b14f23c6e8ce55499f596de8645
DariusDC <47667758+DariusDC@users.noreply.github.com>
2020-05-05 16:00:04 +0300

    Added procfile, gitignore and database

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _  = require('lodash');
 const Request = require('request');
+const mongoose = require('mongoose');
 
 var editText, editTitle;
 var posts = [];
@@ -12,6 +13,9 @@ var shownPost = {
     title: "",
     text : ""
 }
+
+var editedId;
+var isEdited = 0;
 
 const homeStartingContent = "Bine ai venit pe pagina principala a blogului meu, TECH JOURNAL. Acest blog este legat in totalitate de noutatile din domeniul IT din Romania si din restul lumii. Sper sa iti placa aceasta idee si, de asemenea, daca vrei sa aflii mai multe, nu uita sa te abonezi la newsletter-ul meu zilnic, astfel vei primi zilnic noutatile importante de pe blog.";
 const aboutContent = "Salut! Numele meu este Darius Capolna, sunt elev in clasa XII MI la Colegiul National Pedagogic 'Regina Maria' Deva iar acesta este proiectul meu pentru atestatul de informatica. Am o pasiune pentru tot ceea ce tine de informatica, in mod special algoritmica. In clasa a XI-a am reusit sa obtin locul I la Olimpiada Judeteana de Informatica, in clasa a XII reusind mai apoi sa ma calific la faza Nationala. Am realizat acest proiect incercand sa folosesc tot ceea ce am invatat pana la momentul actual legat de programare WEB.";
@@ -24,34 +28,50 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect('mongodb+srv://DariusDC:darkus123@cluster0-yujmk.mongodb.net/blog', {useNewUrlParser: true});
+
+const postSchema = {
+    title: String,
+    text: String
+}
+
+const Post = mongoose.model("Post", postSchema);
+
+const post1 = new Post({
+    title: "Test",
+    text: "Acesta este un test"
+});
+
+// post1.save();
 
 app.get("/", (req, res) => {
 
-    res.render("home", {HomeText: homeStartingContent, posts: posts});
+    Post.find({}, (err, post) => {
+        if (err)
+            console.log(err);
+            
+        res.render("home", {HomeText: homeStartingContent, posts: post});
+    });
+
 });
 
 app.get('/posts/:currentPost', (req, res) => {
-    
-    var found = 0;
-    posts.forEach(element => {
-        if (_.kebabCase(element.title) === _.kebabCase(req.params.currentPost))
-        {
-            found = 1;
-            shownPost = element;
+    Post.findOne({_id: req.params.currentPost}, (err, foundPost) => {
+        if (err){
+            console.log("Not found");
         }
-    })
-    if (found)
-       {
-            res.render('post', {
-                shownPost: shownPost,
-                posts: posts
-            })
+        else {
+            editTitle = foundPost.title;
+            editText = foundPost.text;
+            editedId = foundPost._id;
+            
+            res.render('post', {post: foundPost});
         }
+    });
 });
 
 
 app.get("/compose", (req, res) => {
-    
     res.render("compose", {
         editTitle: "",
         editText: ""
@@ -59,48 +79,76 @@ app.get("/compose", (req, res) => {
 });
 
 
+app.post("/compose", (req, res) => {
+    var newTitle = req.body.titleBox;
+    var newText = req.body.textBox;
+
+    //Adaugam noua postare in baza de date
+    if (!isEdited)
+    {
+        const newPost = new Post({
+            title: newTitle,
+            text: newText
+        });
+        console.log("Nu editam");
+        newPost.save();
+    }
+    else {
+        console.log("Editam");
+        console.log(editedId);
+        
+        Post.findOneAndUpdate(
+            {_id: editedId},
+            {$set: {text: newText, title:newTitle}},
+            (err, doc) => {
+                if (err)
+                    console.log("err");
+                console.log("e ook");
+            }
+        )
+        isEdited = 0;
+    }
+    res.redirect("/");
+    
+});
+
+var editTitle = "";
+var editText = "";
+
+app.get('/compose/edit', (req, res) => {
+    res.render('compose', {editTitle: editTitle, editText: editText});
+});
+
+
  app.post("/edit", (req, res) => {
 
-    for (var i = 0; i < posts.length; i++) {
-        if (_.kebabCase(posts[i].title) ===  _.kebabCase(shownPost.title))
-           {
-            posts.splice(i, 1);
-            console.log("Eliminat la pozitia " + i);
-           }
-    }
-
-     res.render("compose", {
-         editTitle: shownPost.title,
-         editText: shownPost.text 
-     });
-
+    isEdited = 1;
+    res.redirect('compose/edit');
  });
 
 app.post("/delete", (req, res) => {
 
-    for (var i = 0; i < posts.length; i++) {
-        if (_.kebabCase(posts[i].title) ===  _.kebabCase(shownPost.title))
-           {
-            posts.splice(i, 1);
-            console.log("Eliminat la pozitia " + i);
-           }
-    }
+    const currentId = req.body.deleteBTN;
+    
+    Post.findByIdAndDelete(currentId, (err) => {
+        if (err){
+            console.log(err);
+        }
+        else {
+            res.redirect('/');
+        }
+    });
 
-    res.redirect("/");
+    // for (var i = 0; i < posts.length; i++) {
+    //     if (_.kebabCase(posts[i].title) ===  _.kebabCase(shownPost.title))
+    //        {
+    //         posts.splice(i, 1);
+    //         console.log("Eliminat la pozitia " + i);
+    //        }
+    // }
+
 });
 
-app.post("/compose", (req, res) => {
-    
-    var post = {
-      title : req.body.titleBox,
-      text : req.body.textBox
-    }
-
-    posts.push(post);
-    
-    res.redirect("/");
-    
-});
 
 app.get("/contact", (req, res) => {
 
@@ -201,5 +249,4 @@ var PORT = process.env.PORT || 3000;
 app.listen(PORT, function(){
 
     console.log("Server started on port " + PORT);
-    
 });
